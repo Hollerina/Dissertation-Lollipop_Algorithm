cf9341a294044edd334a8741916bc3b3c5205c6a
Lampert <sghlampe@liverpool.ac.uk>
2023-03-03 14:15:14 +0000

    Created the making of graph by generating what chords should connecet to what, as well as seeing if chords are of correct length

---
+++ graph.js
@@ -0,0 +1,28 @@
+/**
+ * @author Holly Lampert
+ * 
+ * @param {Number} num_nodes 
+ * @param {Number} chord_len 
+ * 
+ * 
+ * Will generate a graph based on the number of nodes that have 
+ * been passed, as well using the given chord length that will determine 
+ * the length of the third edge on each node.
+ * 
+ */
+function graph(num_nodes, chord_len){
+    let diagram = [];
+    let num_chords = num_nodes/2;
+
+    //The for loop will create an array which will contain arrays. In the 
+    //contained arrays it should contain the start and end point of a
+    //chord for the graph.
+    for(let i = 0; i < num_chords; i++){
+        diagram.push([(2*i) % num_nodes, ((2*i) + chord_len) % num_nodes]);
+    }
+
+    console.log(diagram);
+
+}
+
+graph(12,3);
---
+++ lollipop.js
@@ -0,0 +1,5 @@
+class Lollipop{
+    constructor(chords){
+        this.chords = chords;
+    }
+}
