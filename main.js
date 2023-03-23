e8f558d968761fba0d280c9e58fa3bb055e54f46
Lampert <sghlampe@liverpool.ac.uk>
2023-03-23 15:40:50 +0000

    Added more UI created a random button

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ let lol;
 let paths;
 let index = 0;
 
-//Get user input of what the chord size will be
-document.getElementById('submit').onclick = () => {
+//need to display correct things depending on which button was clickecd orginally
+document.getElementById("own-input").onclick = own_values;
+document.getElementById("random-input").onclick = random_values;
+
+function own_values(){
+    document.getElementById("own-input").classList.add("hidden");
+    document.getElementById("random-input").classList.add("hidden");
+    document.getElementById("inputs").classList.remove("hidden");
+}
+
+function random_values(){
+    document.getElementById("own-input").classList.add("hidden");
+    document.getElementById("random-input").classList.add("hidden");
+    document.getElementById("random").classList.remove("hidden");
+}
+
+document.getElementById("submit").onclick = begin_algorithm;
+document.getElementById("random-button").onclick = begin_algorithm;
+
+//Get user input of what the chord size will be CHANGE TO FUNCTION RATHER THAN ARROW FUNCTION
+function begin_algorithm(){
     const chord_field = document.getElementById('chord-input');
     const chords = parseInt(chord_field.value);
 
@@ -14,6 +33,7 @@ document.getElementById('submit').onclick = () => {
 
     //remove both fields and just keep inputs
     document.getElementById('inputs').classList.add('hidden');
+    document.getElementById("random").classList.add("hidden");
 
     //Create p elements to display the text
     const newP = document.createElement('p');
@@ -21,27 +41,27 @@ document.getElementById('submit').onclick = () => {
     // document.getElementById
 
     const detailsDiv = document.createElement('p');
-
+    detailsDiv.id ="info";
     detailsDiv.innerHTML = `Node size: ${nodes}  Chord size: ${chords}`
     document.getElementById("details").appendChild(detailsDiv);
 
     //Bring back start button
     document.getElementById("start").classList.remove('hidden');
-
+    document.getElementById("random-button").classList.add("hidden");
     console.log(`Creating graph with nodes ${nodes} and chords ${chords}`)
-    const g = graph(16, 4);
 
-    lol = new Lollipop(g)
-
-    paths = lol.execution(0);
-    lol.print_graph(paths[0] , true);
-
-    window.onresize = () => {
-        lol.print_graph(paths[0] , true)
-    }
 
     try{
+        const g = graph(nodes, chords);
 
+        lol = new Lollipop(g)
+    
+        paths = lol.execution(0);
+        lol.print_graph(paths[0] , true);
+    
+        window.onresize = () => {
+            lol.print_graph(paths[0] , true)
+        }
     
     }
     catch(e){
@@ -64,7 +84,8 @@ function initial_path_finding(){
 
     //Once the simulation is began remove the start button , want to create finish button too
 
-    document.getElementById("start").remove();
+    document.getElementById("start").classList.add("hidden");
+    
     const back = document.createElement("button");
     back.className = "button back_button";
     back.id = "back_button"
@@ -82,6 +103,12 @@ function initial_path_finding(){
     document.getElementById("back_button").onclick = back_path_finding;
 
     const curr_path = document.getElementById("ham-path");
+
+    //Create a finish button which will automatically take you to the last step of the Algorithm
+    document.getElementById("finish").classList.remove("hidden")
+    curr_path.innerHTML = paths[index];
+
+    document.getElementById("finish").onclick = finished;
 
     function next_path_finding(){
         //pass the next Hamiltonian cycle in if the next button is pressed
@@ -106,6 +133,12 @@ function initial_path_finding(){
         }
         console.log(index)
         curr_path.innerHTML = paths[index];
+    }
+
+    function finished(){
+        index = paths.length - 1;
+        lol.print_graph(paths[index]);
+        curr_path.innerHTML = paths[index]
     }
 
 }
